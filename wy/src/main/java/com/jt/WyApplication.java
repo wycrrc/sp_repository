78e891756b71670e08d3205b850b58c4d1b50979
wycrrc <52199190+wycrrc@users.noreply.github.com>
2019-07-01 16:56:38 +0800

    Update WyApplication.java

--- wy/src/main/java/com/jt/WyApplication.java
+++ wy/src/main/java/com/jt/WyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WyApplication {
 	//111111
+       //2222
 	public static void main(String[] args) {
 		SpringApplication.run(WyApplication.class, args);
 	}
