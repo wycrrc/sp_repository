89526a3eab00cc01144617cf1864e211b03d0da7
Administrator <Administrator@QH-20181128INWP>
2019-07-01 16:54:29 +0800

    1

--- wy/src/main/java/com/jt/WyApplication.java
+++ wy/src/main/java/com/jt/WyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WyApplication {
-
+	//111111
 	public static void main(String[] args) {
 		SpringApplication.run(WyApplication.class, args);
 	}
